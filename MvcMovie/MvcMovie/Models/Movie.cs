bb1944698eb9e08d970119b46e615043ea46abf2
eunapark <eapark5908@gmail.com>
2021-11-18 20:10:39 +0900

    닷넷코어 문서 MVC - 검색추가 까지 완료

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -10,9 +8,13 @@ namespace MvcMovie.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
-        public decimal price { get; set; }        
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal price { get; set; }
     }
 }
