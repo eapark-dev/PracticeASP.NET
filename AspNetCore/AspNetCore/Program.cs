06383ce5d853c648eb582f3d615a7a64cfa91b0c
eapark-dev <eapark5908@gmail.com>
2021-12-05 19:01:34 +0900

    ASP.NET CORE MVC 강좌 따라하기

---
+++ AspNetCore/AspNetCore/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCore
+{
+    //Program 거시적인 설정 (Http 서버, IIs 사용 여부 등, 거의 바뀌지 않음)
+    //Startup은 세부적인 설정 (미들웨어 설정, Dependency Injection 등, 필요에 따라 설정)
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 2) IHost를 만든다.
+            // 3) 구동 (Run) < 이때부터 Listen을 시작
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //Host.CreateDefaultBuilder(args)
+        // .ConfigureWebHostDefaults(webBuilder =>
+        //{
+        //  webBuilder.UseStartup<Startup>();
+        // });
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            //1) 각종 옵션 설정 세팅
+            return  Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                // 2) Startup 클래스 지정
+            webBuilder.UseStartup<Startup>();
+            });
+        }
+    }
+}
---
+++ AspNetCore/AspNetCore/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // 각종 서비스 추가 (DI) 영업 시
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            // DI 서비스란 ? SRP (Single Responsiblility Principle)
+            // ex) 랭킹 관련 기능이 필요하면 -> 랭킹 서비스
+        }
+
+        // Http Request Pipeline (NodeJs와 유사)
+        // 미들웨어 : Http request / response를 처리하는 중간 부품
+
+        // [Request]
+        // [파이프라인]
+        // [마지막 Mbc endpoint]
+
+        // 미들웨어에서 처리한 결과물을 다른 미들웨어로 넘길 수 있다.
+        // [파이프라인]
+
+        //[!] Controller에서 처리하지 않는 이유는?
+        //어떤 미들웨어에서 에러가 발생하면 다시 위로 쭉 에러를 전파시킨다.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseExceptionHandler("/Home/Error");
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+            app.UseHttpsRedirection();
+            //Css, Javascript, 이미지 등 요청 받을 때 처
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //라우팅 패턴 설정
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
