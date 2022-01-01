ed84f35985d85bf9d6b083698c03ddc0fe339455
eapark-dev <eapark5908@gmail.com>
2022-01-01 20:43:15 +0900

    ASP.NET Core - 부트스트랩 템플릿 이용한 강좌 (레이아웃)

---
+++ MyWeb.HomeWeb/Controllers/ViewComponents/LeftMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWeb.HomeWeb.Controllers.ViewComponents
+{
+    public class LeftMenu : ViewComponent
+    {
+        public LeftMenu()
+        {
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
