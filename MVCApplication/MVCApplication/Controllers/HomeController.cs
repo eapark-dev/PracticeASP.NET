9194e3f515bfdc29a0190fd0607c076c28e8dd06
eunapark <eapark5908@gmail.com>
2022-09-14 14:56:55 +0900

    asp.net mvc 연습

---
+++ MVCApplication/MVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+          //  throw new Exception("This is unhandled exception");
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HandleError]
+        public ActionResult Contact()
+        {
+            string msg = null;
+            ViewBag.Message = msg.Length;
+
+            return View();
+        }
+    }
+}
---
+++ MVCApplication/MVCApplication/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using MVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        static IList<Student> studentList = new List<Student>{
+                new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+            };
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(studentList);
+        }
+
+        public ActionResult Edit()
+        {
+            // do something here
+            return View();
+        }
+
+
+        //[Bind(Include = "StudentId, StudentName")]  -> 필요항목만 바인딩 하기 
+        //[Bind(Exclude = "Age")] -> 필요없는 항목만 제외하고 바인딩 하기
+        [HttpPost]
+        public ActionResult Edit(int Id)
+        {
+            //here, get the student from the database in the real application
+
+            //getting a student from collection for demo purpose
+            var std = studentList.Where(s => s.StudentId == Id).FirstOrDefault();
+
+            return View(std);
+        }
+    }
+}
